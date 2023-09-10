9bb5a043f20c3a0c35ad036bfda1ec35bf9b5ab7
seoeuncho <westsilver24@naver.com>
2023-09-11 01:31:15 +0900

    commit

--- 1_react/react-tictactoe-app/src/components/Board.js
+++ 1_react/react-tictactoe-app/src/components/Board.js
@@ -54,23 +54,25 @@ const Board = () => {
   }
 
   return (
-    <div className='board-wrapper'>
+    <div>
+      <div className='board-wrapper'>
+        <div className='board-row'>
+          {renderSquare(0)}
+          {renderSquare(1)}
+          {renderSquare(2)}
+        </div>
+        <div className='board-row'>
+          {renderSquare(3)}
+          {renderSquare(4)}
+          {renderSquare(5)}
+        </div>
+        <div className='board-row'>
+          {renderSquare(6)}
+          {renderSquare(7)}
+          {renderSquare(8)}
+        </div>
+      </div>
       <div className='status'>{ status }</div>
-      <div className='board-row'>
-        {renderSquare(0)}
-        {renderSquare(1)}
-        {renderSquare(2)}
-      </div>
-      <div className='board-row'>
-        {renderSquare(3)}
-        {renderSquare(4)}
-        {renderSquare(5)}
-      </div>
-      <div className='board-row'>
-        {renderSquare(6)}
-        {renderSquare(7)}
-        {renderSquare(8)}
-      </div>
     </div>
   )
 }
