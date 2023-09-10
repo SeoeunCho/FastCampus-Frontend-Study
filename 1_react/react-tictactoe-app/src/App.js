fb6f7c3c90d7d27b01157df37caf1c5e5f433969
seoeuncho <westsilver24@naver.com>
2023-09-11 01:03:12 +0900

    initial commit

---
+++ 1_react/react-tictactoe-app/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Board from "./components/Board";
+
+function App() {
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board />
+      </div>
+      <div className="game-info">
+        <div>get info</div>
+        <ol></ol>
+      </div>
+    </div>
+  );
+}
+
+export default App;
