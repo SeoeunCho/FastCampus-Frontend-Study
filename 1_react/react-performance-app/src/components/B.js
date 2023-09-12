fd5c38864ece709c465277ccf99be9fd65707751
seoeuncho <westsilver24@naver.com>
2023-09-12 19:32:20 +0900

    commit

--- 1_react/react-performance-app/src/components/B.js
+++ 1_react/react-performance-app/src/components/B.js
@@ -1,9 +1,35 @@
 import React from 'react'
 
-const B = () => {
-  return (
-    <div>B</div>
-  )
+const Message = ({ message }) => {
+  return <p>{message}</p>;
 }
 
-export default B
+const ListItem = ({ post }) => {
+  return (
+    <li key={post.id}>
+      <p>{post.title}</p>
+    </li>
+  );
+}
+
+const List = ({ posts }) => {
+  return (
+    <ul>
+      {posts.map(post => (
+        <ListItem key={post.id} post={post} />
+      ))}
+    </ul>
+  );
+}
+
+const B = ({ message, posts }) => {
+  return (
+    <div>
+      <h1>B Component</h1>
+      <Message message={message} />
+      <List posts={posts} />
+    </div>
+  );
+};
+
+export default B;
